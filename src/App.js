d537db08233ae83d1e1bc3e77e3816a270c4ffd6
mhmeroglu <mhmeroglu@gmail.com>
2022-08-27 20:48:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { createContext, useState } from "react";
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "love" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <h1> {theme === "light" ? "Aşkımmmm Kalbime Tıklar Mısın ?" : "Sekizinci Ayımız Kutlu Olsun Bebeğimmm. Seninle Geçirdiğim Her Vakit Sana Olan Sevgim Katlanarak Arttı ve Artmaya Devam Ediyor. Seninle Olduğum İçin Kendimi Çok Mutlu ve Şanslı Hissediyorum. Nice Birlikte Yıllara. İyi ki Varsın Hayatımın Anlamı. Seni Çoooooook Seviyorummmmm <333"}</h1>
+        <button onClick={toggleTheme} checked={theme === "love"}>
+        <FontAwesomeIcon icon={faHeart} />
+        </button>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
