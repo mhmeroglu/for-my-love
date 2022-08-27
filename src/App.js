45efa5dbd49b454592888d1e024987bf88aa5668
mhmeroglu <mhmeroglu@gmail.com>
2022-08-27 21:37:19 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
-        <h1> {theme === "light" ? "Aşkımmmm Kalbime Tıklar Mısın ?" : "Sekizinci Ayımız Kutlu Olsun Bebeğimmm. Seninle Geçirdiğim Her Vakit Sana Olan Sevgim Katlanarak Arttı ve Artmaya Devam Ediyor. Seninle Olduğum İçin Kendimi Çok Mutlu ve Şanslı Hissediyorum. Nice Birlikte Yıllara. İyi ki Varsın Hayatımın Anlamı. Seni Çoooooook Seviyorummmmm <333"}</h1>
+        <h1> {theme === "light" ? "Aşkımmmm Kalbime Tıklar Mısın ?" : "Sekizinci Ayımız Kutlu Olsun Bebeğimmm. Seninle Geçirdiğim Her Vakit Sana Olan Sevgim Katlanarak Arttı ve Artmaya Devam Ediyor. Seninle Olduğum İçin Kendimi Çok Mutlu ve Şanslı Hissediyorum. Beraber Nice Uzun Yıllara Sevgilim. İyi ki Varsın Hayatımın Anlamı. Seni Çoooooook Seviyorummmmm <333"}</h1>
         <FontAwesomeIcon 
         id='heart'
         icon={faHeart} 
